b9fb9995024c1a900c6c8c858a1ce5eedf00ed45
Henrique <henrique.c.grecco@outlook.com>
2024-08-30 02:09:57 -0300

    update java class

--- desafio-banco-terminal/src/ContaTerminal.java
+++ desafio-banco-terminal/src/ContaTerminal.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) {
 
-        try (Scanner scan = new Scanner(System.in)) {
+        // Usando um try para evitar alerta de recurso
+        try (Scanner scan = new Scanner(System.in)) { // Criando uma instancia de um objeto da classe Scanner
 
-            System.out.println("Por favor, informe a agencia: ");
-            int agencia = Integer.valueOf(scan.nextLine());
-
+            System.out.println("Por favor, informe a agencia: "); // Solicitando informação para o usuario
+            int agencia = Integer.valueOf(scan.nextLine()); /* Recebendo informação solicitada do tipo String
+                                                             e convertendo para o tipo int*/
             System.out.println("Por favor, informe o número da conta com o dígito verificador: ");
             String numeroDaConta = scan.nextLine();
 
@@ -16,10 +17,9 @@ public class ContaTerminal {
             System.out.println("Por favor, informe o saldo para saque: ");
             double saldo = Double.valueOf(scan.nextLine());
 
-            System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agencia é " + agencia +
-            ", conta " + numeroDaConta + " e seu saldo " + saldo + " já está disponível para saque.");
-            
-        }
+            System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agencia é "
+            + agencia + ", conta " + numeroDaConta + " e seu saldo " + saldo + " já está disponível para saque.");
 
+        }
     }
 }
