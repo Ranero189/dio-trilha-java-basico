d8ba43afdd35c3d263c27115aad2413128cc1599
Henrique <henrique.c.grecco@outlook.com>
2024-08-30 01:58:40 -0300

    update java class

--- desafio-banco-terminal/src/ContaTerminal.java
+++ desafio-banco-terminal/src/ContaTerminal.java
@@ -18,6 +18,7 @@ public class ContaTerminal {
 
             System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agencia é " + agencia +
             ", conta " + numeroDaConta + " e seu saldo " + saldo + " já está disponível para saque.");
+            
         }
 
     }
